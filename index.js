2dc754889b666aa6ecbb6e6595860acce9e1c3a0
Nishant <nishantsingh2jan1998@gmail.com>
2020-07-11 15:32:56 +0530

    Add all my files`

---
+++ index.js
@@ -0,0 +1,18 @@
+const http=require('http');
+const fs=require('fs');
+const path=require('path');
+
+const hostname="localhost";
+const port=3000;
+const server=http.createServer((req,res)=>{
+console.log(req.headers);
+res.statusCode=200;
+res.setHeader('Content-Type','text/html');
+res.end(`<html><body><h1>Connection Successful :)</h1></body></html>`)
+
+});
+server.listen(port,hostname,()=>{
+
+	console.log(`server runniing at http://${hostname}:${port}`);
+
+});
